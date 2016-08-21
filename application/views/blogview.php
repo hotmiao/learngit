7335a66d6e6e79eef8ec9127fa5e9273d77a4305
MiaoDavid <David@MiaoDaviddeMacBook-Pro.local>
2016-08-21 20:34:02 +0800

    1

--- application/views/blogview.php
+++ application/views/blogview.php
@@ -6,7 +6,7 @@
 <h1>Welcome to my Blog!</h1>
 <ul>
     <?php foreach($query as $list):?>
-        <li><?php echo $list->document_id;?></li>
+        <li><?=$list->document_id?></li>
         <li><?php echo $list->title;?></li>
         <li><?php echo $list->content;?></li>
         <li><?php echo $list->if_deleted;?></li>
