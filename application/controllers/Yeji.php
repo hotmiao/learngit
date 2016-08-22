845aabd852cfc220e0a2c7d649e252985e06fcad
MiaoDavid <David@MiaoDaviddeMacBook-Pro.local>
2016-08-22 21:49:37 +0800

    1

--- application/controllers/Yeji.php
+++ application/controllers/Yeji.php
@@ -3,14 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Yeji extends CI_Controller {
 
-    public function index()
+    public function yeji_add()
     {
         $this->load->view('yejiadd');
-    }
-
-    public function yeji()
-    {
-//        $price = $_POST['price'];
-//        print_r($price);
     }
 }
