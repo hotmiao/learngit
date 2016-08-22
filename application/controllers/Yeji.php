852b89df1f6a4434f2659cb8f74d4a7eafd8686d
MiaoDavid <David@MiaoDaviddeMacBook-Pro.local>
2016-08-22 21:31:39 +0800

    1

--- application/controllers/Yeji.php
+++ application/controllers/Yeji.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: David
- * Date: 16-8-22
- * Time: 下午8:29
- */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Yeji extends CI_Controller {
