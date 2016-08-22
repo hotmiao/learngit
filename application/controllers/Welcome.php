9df4e8cf1c65326a3fe8dbc7f38bd4b0c6b26549
MiaoDavid <David@MiaoDaviddeMacBook-Pro.local>
2016-08-22 21:26:14 +0800

    1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-//		$this->load->view('welcome_message');
-        echo 'Hello World1';
+		$this->load->view('welcome_message');
+//        echo 'Hello World1';
 	}
 }
